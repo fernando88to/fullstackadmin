2b3b3ab16e690e83a70473564e98029faf7b0248
fernando <fernando88to@gmail.com>
2023-01-04 14:45:47 -0300

    desabilitar o spinner do nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,34 @@
 import * as React from 'react';
 import Head from 'next/head';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
+import {AppProps} from 'next/app';
+import {ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
+import {CacheProvider, EmotionCache} from '@emotion/react';
 import theme from '../theme';
 import createEmotionCache from '../createEmotionCache';
 import {ColorProviderWrapper} from "../context/ColorModeContext";
+import {Router} from "next/router";
+
+import NProgress from 'nprogress';
+//import do css do nprogress
+import "nprogress/nprogress.css";
+
+
+NProgress.configure({showSpinner: false});
+/**
+ * toda vez que a rota for alterada
+ */
+Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+    NProgress.done();
+});
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -16,20 +38,36 @@ interface MyAppProps extends AppProps {
 }
 
 
-
-
 export default function MyApp(props: MyAppProps) {
-    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
     return (
         <CacheProvider value={emotionCache}>
             <Head>
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
             </Head>
             <ColorProviderWrapper>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
+                <CssBaseline/>
                 <Component {...pageProps} />
             </ColorProviderWrapper>
+
+            {/*global indica que vai ser em todas as paginas*/}
+            <style global jsx>
+                {
+                    `
+
+                      #nprogress {
+                        position: relative;
+                        z-index: 9999999;
+                      }
+
+                      #nprogress .bar {
+                        background: #959ed6 !important;
+                        height: 3px;
+                      }
+                    `
+                }
+            </style>
 
         </CacheProvider>
     );
---
+++ src/pages/pagina3.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import {Layout} from "../components/layout";
+import {GetServerSideProps} from "next";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const future = Date.now() + 6000;
+    while (Date.now() < future) {
+    }
+    return {
+        props: {},
+    }
+};
+export default function About() {
+
+
+    return (
+        <Layout>
+            <p>
+                Pagina tres
+            </p>
+        </Layout>
+    );
+}
