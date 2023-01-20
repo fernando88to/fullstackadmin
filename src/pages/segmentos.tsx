3d5fab62594083c5b4013cd8cdadad92003f7713
fernando <fernando88to@gmail.com>
2023-01-20 08:36:58 -0300

    criação de uma pagina com static revalidate

--- src/pages/segmentos.tsx
+++ src/pages/segmentos.tsx
@@ -1,9 +1,23 @@
 import {Layout} from "../components/layout";
 import {Header} from "../components/Header";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Segmento} from "../types/Segmento";
 import {clientBackend} from "../clients/clientBackend";
+import {CircularIndeterminate} from "../components/Progress";
 
+
+const ListSegmento: React.FC<{ data: Segmento[] }> = (props) => {
+    return (
+        <ul>
+            {props.data.map(segmento => {
+                return (<li key={segmento.codigo}>
+                    {segmento.nome}
+                </li>);
+            })}
+
+        </ul>
+    );
+}
 export default function Page() {
     const [data, setData] = useState<Segmento[]>([]);
     const [loading, setLoading] = useState(true);
@@ -21,15 +35,7 @@ export default function Page() {
         <Layout>
             <Header title="Segmentos" subTitle="Segue abaixo os segmentos dos cartórios extra judiciais juntamente
             com um resumo"/>
-            {!loading && <ul>
-                {data.map(segmento => {
-                    return (<li key={segmento.codigo}>
-                        {segmento.nome}
-                    </li>);
-                })}
-
-            </ul>
-            }
+            {loading ? <CircularIndeterminate/> : <ListSegmento data={data}></ListSegmento>}
 
         </Layout>
     );
---
+++ src/pages/segmentosStaticos.tsx
@@ -0,0 +1,52 @@
+import {Layout} from "../components/layout";
+import {Header} from "../components/Header";
+import React from "react";
+import {Segmento} from "../types/Segmento";
+import {GetStaticProps} from "next";
+import {mongoServiceSegmentos} from "../databases/mongoService";
+
+
+type Props = {
+    data: Segmento[]
+};
+export const getStaticProps: GetStaticProps = async (context) => {
+
+    let allSegmentos: Segmento[] = await mongoServiceSegmentos.getAll();
+    console.log(allSegmentos);
+    return {
+        props: {
+            data:JSON.parse(JSON.stringify(allSegmentos))
+        },
+        // Next.js will attempt to re-generate the page:
+        // - When a request comes in
+        // - At most once every 10 seconds
+        revalidate: 30, // In seconds
+    }
+}
+const ListSegmento: React.FC<Props> = (props) => {
+    return (
+        <ul>
+            {props.data.map(segmento => {
+                return (<li key={segmento.codigo}>
+                    {segmento.nome}
+                </li>);
+            })}
+
+        </ul>
+    );
+}
+
+
+export default function Page(props:Props) {
+
+
+
+    return (
+        <Layout>
+            <Header title="Segmentos" subTitle="Segue abaixo os segmentos dos cartórios extra judiciais juntamente
+            com um resumo"/>
+             <ListSegmento data={props.data}></ListSegmento>
+
+        </Layout>
+    );
+}
