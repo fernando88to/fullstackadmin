6497f70a3bdb51fb3ddf186d08d58733544708e1
fernando <fernando88to@gmail.com>
2023-01-22 22:09:34 -0300

    refatorando o login

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import {LayoutDashboard} from "@/components/layout";
+import {GetServerSideProps} from "next";
 
 
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+    return {
+        props: {}, // will be passed to the page component as props
+    }
+}
 /*const Item = styled(Paper)(({theme}) => ({
 
     ...theme.typography.body2,
