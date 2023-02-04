cb22f41a8239abd9a8cf160a307b770af27544fe
fernando <fernando88to@gmail.com>
2023-02-04 19:11:58 -0300

    ajuste na seleção do menu selecionado

--- src/pages/api/segmentos/[id].ts
+++ src/pages/api/segmentos/[id].ts
@@ -9,20 +9,26 @@ import {sleep} from "@/core/Sleep";
 export default withAuthenticatedSession(handler)
 
 
-export  async function handler(req: NextApiRequest, res: NextApiResponse<Segmento | {}>) {
+export async function handler(req: NextApiRequest, res: NextApiResponse<Segmento | {}>) {
     const {method} = req;
     const {id} = req.query;
     switch (method) {
         case 'GET':
             const segmentoInstance = await mongoServiceSegmentos.getSegmentoByCodigo(Number(id));
-            if(segmentoInstance){
+            if (segmentoInstance) {
                 res.status(200).json(segmentoInstance);
                 return;
             }
             res.status(404).json({});
             break;
+        case 'PUT':
+            const segmentoByUser: Segmento = req.body as Segmento;
+            console.log(segmentoByUser);
+            res.status(204).json(segmentoByUser);
+            break;
+
         default:
-            res.setHeader('Allow', ['GET'])
+            res.setHeader('Allow', ['GET','PUT'])
             res.status(405).end(`Method ${method} Not Allowed`)
     }
 
--- src/pages/dashboard/segmentos/edit/[id].tsx
+++ src/pages/dashboard/segmentos/edit/[id].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import {LayoutDashboard} from "@/components/layout";
 import {Header} from "@/components/Header";
 import {Segmento} from "@/types/Segmento";
@@ -6,12 +6,13 @@ import {useGetSegmentoByCodigo} from "@/hooks/segmentoHooks";
 import {Container} from "@/components/Container";
 import {useRouter} from "next/router";
 import {CircularIndeterminate} from "@/components/Progress";
-import {TextField} from "@mui/material";
+import {Alert, Stack, TextField} from "@mui/material";
 import {BotaoVoltar} from "@/components/navegation/BotaoVoltar";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import {BoxButtonRight} from "@/components/BoxButtonRight";
 import Button from "@mui/material/Button";
 import SaveIcon from '@mui/icons-material/Save';
+import {width} from "@mui/system";
 
 
 type Props = {
@@ -19,12 +20,48 @@ type Props = {
 };
 
 const FormularioEdicaoSegmento: React.FC<{ segmento: Segmento }> = ({segmento}) => {
+    const codigoInput = useRef<HTMLInputElement>(null);
+    const nomeInput = useRef<HTMLInputElement>(null);
+    const resumoInput = useRef<HTMLInputElement>(null);
+    const [errosList, setErrosList] = useState<string[]>([]);
+
+    const validarCampos = (segmento: Segmento) => {
+        let erros: string[] = [];
+        if (!segmento.codigo) {
+            erros.push('Informe o campo codigo');
+        }
+        if (!segmento.nome) {
+            erros.push('Informe o campo nome');
+        }
+        if (!segmento.resumo) {
+            erros.push('Informe o campo resumo');
+        }
+        setErrosList(erros);
+    }
+    const handleSubmitForm = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        const codigo = Number(codigoInput.current?.value);
+        const nome = nomeInput.current?.value || '';
+        const resumo = resumoInput.current?.value || '';
+        let segmento = {codigo: codigo, nome: nome, resumo: resumo} as Segmento;
+        validarCampos(segmento);
+    }
+
     return (
         <>
+            <Grid2 xs={12} md={12}>
+                <Stack sx={{width: '100%'}} spacing={2}>
+                    {errosList.map((erro) => {
+                        return (<Alert key={erro} severity="error">{erro}</Alert>);
+                    })}
+                </Stack>
+            </Grid2>
 
-            <Grid2 xs={12} md={6} >
+            <Grid2 xs={12} md={6}>
                 <TextField variant="outlined" id="codigo" aria-describedby="my-helper-text"
                            label="Código"
+                           name="codigo"
+                           inputRef={codigoInput}
                            value={segmento.codigo}
                            InputProps={{
                                readOnly: true,
@@ -35,18 +72,19 @@ const FormularioEdicaoSegmento: React.FC<{ segmento: Segmento }> = ({segmento}) 
 
             </Grid2>
             <Grid2 xs={12} md={6}>
-                <TextField variant="outlined" id="codigo" aria-describedby="my-helper-text"
+                <TextField variant="outlined" id="nome" name="nome" aria-describedby="my-helper-text"
                            label="Nome"
-                           value={segmento.nome}
+                           inputRef={nomeInput}
+                           defaultValue={segmento.nome}
                            fullWidth
                            autoFocus
                            helperText="Nome do segmento "/>
 
             </Grid2>
-            <Grid2 xs={12} md={12} >
-                <TextField variant="outlined" id="codigo" aria-describedby="my-helper-text"
+            <Grid2 xs={12} md={12}>
+                <TextField variant="outlined" id="resumo" name="resumo" aria-describedby="my-helper-text"
                            label="Resumo"
-                           value={segmento.resumo}
+                           defaultValue={segmento.resumo}
                            fullWidth
                            multiline
                            rows={10}
@@ -54,12 +92,10 @@ const FormularioEdicaoSegmento: React.FC<{ segmento: Segmento }> = ({segmento}) 
             </Grid2>
             <BoxButtonRight>
                 <Button variant="contained"
-                        startIcon={<SaveIcon />}>atualizar</Button>
+                        onClick={handleSubmitForm}
+                        startIcon={<SaveIcon/>}>atualizar</Button>
             </BoxButtonRight>
-
-
         </>
-
 
 
     );
@@ -72,15 +108,12 @@ export default function Page() {
     const {data, error} = useGetSegmentoByCodigo(Number(id));
 
 
-
     return (
-        <LayoutDashboard itemMenuSelected={9}>
+        <LayoutDashboard itemMenuSelected={11}>
             <Header title="Segmentos" subTitle="Segue abaixo os segmentos dos cartórios extra judiciais juntamente
             com um resumo"/>
             <Container>
-                {/*<Grid2 xs={12} container justifyContent="flex-end">
-                    <BotaoVoltar url="/dashboard/segmentos/segmentosSwr"/>
-                </Grid2>*/}
+
                 <BoxButtonRight>
                     <BotaoVoltar url="/dashboard/segmentos/segmentosSwr"/>
                 </BoxButtonRight>
